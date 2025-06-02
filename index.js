538e32157b765e512110a7918f5beecefb272fba
Celeste <103224302+celestedelossantos@users.noreply.github.com>
2025-06-02 13:59:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const { 0: method, 1: endpoint, 2: title, 3: price, 4: category } = process.argv
 
 const handlerMethod = {
     [METHODS.GET]: ({endpoint}) => getProducts(endpoint),
-    [METHODS.POST]: async ({title, category, price}) => await postProduct(title, price, category),
-    [METHODS.DELETE]: async ({endpoint}) => await deleteProduct(endpoint)
+    [METHODS.POST]: ({title, category, price}) => postProduct(title, price, category),
+    [METHODS.DELETE]: ({endpoint}) => deleteProduct(endpoint)
 }
 
 const { data, status, request, statusText } = await handlerMethod[method]({endpoint, title, category, price})
