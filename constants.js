2ffe9fd9d42c7d33a9a165fab954d6ed1d63cc1f
Celeste <103224302+celestedelossantos@users.noreply.github.com>
2025-06-02 13:51:54 -0300

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,11 @@
+const API_BASE = "https://fakestoreapi.com/"
+
+export const API = {
+    product: API_BASE + "products/"
+}
+
+export const METHODS = {
+    'GET': 'GET',
+    'POST': 'POST',
+    'DELETE': 'DELETE'
+}
