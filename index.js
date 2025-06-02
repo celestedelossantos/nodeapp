95cea951b05e5fcf8c68e98c51086a199b50d389
Celeste <103224302+celestedelossantos@users.noreply.github.com>
2025-06-02 13:52:25 -0300

    pre entrega

---
+++ index.js
@@ -0,0 +1,13 @@
+import { METHODS } from "./constants.js"
+import { deleteProduct, getProducts, postProduct } from "./handleMethod.js"
+
+const { 0: method, 1: endpoint, 2: title, 3: price, 4: category } = process.argv.slice(2)
+
+const handlerMethod = {
+    [METHODS.GET]: ({endpoint}) => getProducts(endpoint),
+    [METHODS.POST]: async ({title, category, price}) => await postProduct(title, price, category),
+    [METHODS.DELETE]: async ({endpoint}) => await deleteProduct(endpoint)
+}
+
+const { data, status, request, statusText } = await handlerMethod[method]({endpoint, title, category, price})
+console.log(`🚀 ${request.method} => ${status} ~ ${statusText}:`, data)
